f5c7d54e6a9d662bb0cd88b678c5199d76d2b076
Tatiana Sorokina <tssorokina@edu.hse.ru>
2025-05-29 13:19:52 +0200

    Deploy new Next.js static site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   trailingSlash: true,
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
---
+++ out/about.md
@@ -0,0 +1,3 @@
+# Intro
+
+I am a Quantitative and AI researcher with background in Statistics, Data Science, Macroeconomic Theory, and Credit Risk Management.
